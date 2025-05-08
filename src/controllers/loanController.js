d2a7b73fcc6352afedc821ec6ea00084da0af87e
sinha40 <sheshant.sinha@gmail.com>
2025-05-08 22:44:51 +0530

    removed express validator

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -1,14 +1,8 @@
-const { validationResult } = require('express-validator');
 const LoanService = require('../services/loanService');
 
 class LoanController {
     static async createLoanApplication(req, res) {
         try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-            }
-
             const applicationData = {
                 customer_id: req.body.customer_id,
                 amount: parseFloat(req.body.amount),
