7bfe25545d70e3bcdc680715fdc3de9e93037f2b
sinha40 <sheshant.sinha@gmail.com>
2025-05-08 20:46:50 +0530

    added port

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ app.use((req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Initialize database and start server
 async function startServer() {
